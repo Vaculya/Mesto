7f5e4c2dde2ebb938b86d482ee2f2cb4fd099a15
Vladislav Matveev <matveevvlad20017@gmail.com>
2022-11-29 14:33:39 +0300

    fix: fix popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,17 @@ let buttonEditFormOpen = document.querySelector(".profile__edit-btn"),
     popup = document.querySelector(".popup"),
     popupClose = document.querySelector(".popup__close"),
     formElement = document.querySelector(".popup__form"),// Форма
-    nameInput = document.querySelector(".popup__input_name"), // имя в попап
-    jobInput = document.querySelector(".popup__input_about"),// о человеке в попап
+    nameInput = document.querySelector(".popup__input_info_name"), // имя в попап
+    jobInput = document.querySelector(".popup__input_info_about"),// о человеке в попап
     profileName = document.querySelector(".profile__name"), // Имя профайла
     profileAbout = document.querySelector(".profile__about"); // О человеке
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileAbout.textContent;
+
 
 
 function openPopup (){
   popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
 }
 
 function closePopup(){
