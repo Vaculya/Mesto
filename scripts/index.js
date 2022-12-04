0463dac6c0ee4cdd9b246538ee99c8eadc481553
Vladislav Matveev <matveevvlad20017@gmail.com>
2022-12-05 02:09:08 +0300

    feat: add cardsOnLoad

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,19 @@ let buttonEditFormOpen = document.querySelector(".profile__edit-btn"),
     profileName = document.querySelector(".profile__name"), // Имя профайла
     profileAbout = document.querySelector(".profile__about"); // О человеке
 
-
-
+// Open profile popup
 function openPopup (){
   popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
 }
 
+// close popup
 function closePopup(){
   popup.classList.remove("popup_opened");
 }
 
+// save profile popup
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
@@ -33,3 +34,44 @@ formElement.addEventListener('submit', formSubmitHandler);
 buttonEditFormOpen.addEventListener("click", openPopup);
 
 popupClose.addEventListener("click", closePopup);
+
+let elements = document.querySelector('.elements');
+
+let initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementsLoad = initialCards.forEach(element =>{
+
+  const elementsTemplate = document.querySelector('#element-template').content;
+  const elementsItem = elementsTemplate.querySelector('.elements__item').cloneNode(true);
+
+  elementsItem.querySelector('.elements__img').setAttribute('src', element.link);
+  elementsItem.querySelector('.elements__name').textContent = element.name;
+
+  elements.append(elementsItem);
+});
+
