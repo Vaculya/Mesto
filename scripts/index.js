0aa78bec437e53e49c9b79dac7276bcd0510134b
Vladislav Matveev <matveevvlad20017@gmail.com>
2022-12-05 16:34:26 +0300

    feat: add card adding

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,45 @@
 let buttonEditFormOpen = document.querySelector(".profile__edit-btn"),
     popup = document.querySelector(".popup"),
+    popupEdit = document.querySelector('.popup_type_edit'),
     popupClose = document.querySelector(".popup__close"),
-    formElement = document.querySelector(".popup__form"),// Форма
+    formEdit = document.querySelector(".popup__form_type_edit"),// Форма изменения профиля
     nameInput = document.querySelector(".popup__input_info_name"), // имя в попап
     jobInput = document.querySelector(".popup__input_info_about"),// о человеке в попап
     profileName = document.querySelector(".profile__name"), // Имя профайла
-    profileAbout = document.querySelector(".profile__about"); // О человеке
+    profileAbout = document.querySelector(".profile__about"), // О человеке
+
+    buttonProfileaAdd = document.querySelector('.profile__add-btn'); // кнопка открытия попап с фото
+    popupAdd = document.querySelector('.popup_type_add'), //попап добавления фото
+    closeBtnAddPopup = document.querySelector('.popup__close_btn-add'),
+    formAdd = document.querySelector('.popup__form_type_add'), // форма добавления места
+    placeName = document.querySelector('.popup__input_place_name'), // Название места в попап
+    imgLink = document.querySelector('.popup__input_img_link'); //Ссылка на картинку в попап
 
 // Open profile popup
-function openPopup (){
-  popup.classList.add("popup_opened");
+// function openPopup (){
+//   popup.classList.add("popup_opened");
+//   nameInput.value = profileName.textContent;
+//   jobInput.value = profileAbout.textContent;
+// }
+
+
+function openPopupEdit (){
+  popupEdit.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
+}
+
+function openPopupAdd(){
+  popupAdd.classList.add("popup_opened");
 }
 
 // close popup
 function closePopup(){
   popup.classList.remove("popup_opened");
+}
+
+function closeAddPopup(){
+  popupAdd.classList.remove("popup_opened");
 }
 
 // save profile popup
@@ -27,13 +50,9 @@ function formSubmitHandler (evt) {
     profileAbout.textContent = jobInput.value;
 
     closePopup();
-}
+};
 
-formElement.addEventListener('submit', formSubmitHandler);
-
-buttonEditFormOpen.addEventListener("click", openPopup);
-
-popupClose.addEventListener("click", closePopup);
+//Добавление фото на страницу
 
 let elements = document.querySelector('.elements');
 
@@ -64,14 +83,48 @@ let initialCards = [
   }
 ];
 
-const elementsLoad = initialCards.forEach(element =>{
+//функция для постраения карточек по всему массиву
+function AddElement (){
+  initialCards.forEach(element => {
 
-  const elementsTemplate = document.querySelector('#element-template').content;
-  const elementsItem = elementsTemplate.querySelector('.elements__item').cloneNode(true);
+    const elementsTemplate = document.querySelector('#element-template').content;
+    const elementsItem = elementsTemplate.querySelector('.elements__item').cloneNode(true);
 
-  elementsItem.querySelector('.elements__img').setAttribute('src', element.link);
-  elementsItem.querySelector('.elements__name').textContent = element.name;
+    elementsItem.querySelector('.elements__img').setAttribute('src', element.link);
+    elementsItem.querySelector('.elements__img').setAttribute('alt', element.name);
+    elementsItem.querySelector('.elements__name').textContent = element.name;
 
-  elements.append(elementsItem);
-});
+    elements.append(elementsItem);
+  });
+};
 
+const addNewPlace = function(evt) {
+  evt.preventDefault();
+  // добавление в массив нового оъекта
+  initialCards.unshift(
+    {
+      name: placeName.value,
+      link: imgLink.value
+    }
+  );
+
+  elements.innerHTML = '';
+  AddElement();
+  closeAddPopup();
+};
+
+
+
+buttonEditFormOpen.addEventListener("click", openPopupEdit); // открытие попап изменение профиля
+
+formEdit.addEventListener('submit', formSubmitHandler); //при нажатии сохранить в изменении профиля
+
+popupClose.addEventListener("click", closePopup); // Закрыть попап редактирования профиля
+
+buttonProfileaAdd.addEventListener('click', openPopupAdd); // открыть попап добавления фото
+
+formAdd.addEventListener('submit', addNewPlace); // добавить новое фото и закрыть попап
+
+closeBtnAddPopup.addEventListener("click", closeAddPopup); // закрыть попап добавления фото
+
+window.onload = AddElement(); // закгрузка карточек при открытие страницы
